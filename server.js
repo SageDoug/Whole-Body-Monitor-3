331232acb6a1d5cf674d676d325809eb6c6d0d57
SageDoug <26abrennell@vianney.com>
2026-02-17 11:14:09 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const roleRoutes = require("./routes/roleRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api", roleRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
